2a7f8feee22c06e4d3b502a7dcc0aa9643636d34
Serguei Cambour <s.cambour@gmail.com>
2020-12-03 11:42:16 +0100

    Add null: false to Product#title

--- db/migrate/20201203103853_create_posts.rb
+++ db/migrate/20201203103853_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :body
 
       t.timestamps
